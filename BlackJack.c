157c5c97b88811c7ca558d348aa790810a2bd77e
jeremy-simon <jokoast@gmail.com>
2014-11-28 12:17:42 +0100

    upd8 engine

--- BlackJack.c
+++ BlackJack.c
@@ -15,85 +15,93 @@
 #include "includes/BlackJack.h"
 
 // initialisation du jeu de cartes a un etat 0 : la carte est piochable
-void init(){
-	int i;
-	for (i = 0; i < 52; i++){
-	
-		if (i < 12){
-			mainjoueur[i].valeur = nocarte;
-			mainjoueur[i].couleur = nocouleur;
-			
-			mainbanque[i].valeur = nocarte;
-			mainbanque[i].couleur = nocouleur;
-		}
-		
-		cartes[i] = 0;
-		
+void init_game(){
+	int i = 0;
+	mainjoueur.indice=-1;
+	mainbanque.indice=-1;
+	for (i=0 ; i < 52 ; i++){
+		cartes[i]=0;
 	}
+	fprintf(stdout,"\nPaquet et mains initialisés");
 }
 
-// 
+// Fonction qui ajoute une carte à la main ()
+void ajout(t_main * joueur, int valeur, int couleur){
+	joueur->indice = joueur.indice + 1;
+	joueur->carte.valeur = valeur;
+	joueur->carte.couleur = carte;
+}
 
 // Fonction qui génère une carte aléatoire (carte)
 short carte_aleatoire(){
-	
-	srand(time(NULL));
-	
-	return (rand() % 52);
+	return (rand()+1 % 52);
 }
 
 // Retourne une carte encore non utilisée
-short tirer_carte(short joueur, t_main main){
+short tirer_carte(short joueur){
 
+	int tvaleur;
+	int tcouleur;
 	short carte = carte_aleatoire();
 	
-	/*while (tas[carte]!=0){
+	// Tant que la carte n'est pas libre
+	while (cartes[carte]!=LIBRE){
 		carte = carte_aleatoire();
 	}
+
+	// Calcul de la valeur de la carte et sa couleur
+	tvaleur=carte%13;
+	tcouleur=carte/13;
+
 	
-	cartes[carte]=joueur;
-	main[(main[0].valeur)+1].valeur=carte%13;
-	main[(main[0].valeur)+1].couleur=carte/13;
-	main[0].valeur += 1;
-	*/
+	// On attribue la carte à la bonne main
+	if(joueur==BANQUE){
+		if(joueur->indice == -1){
+			cartes[carte]=1;
+		}
+		else cartes[carte]=2; // RAJOUTER LA FONCTION CARTE CACHEE
+		ajout(mainbanque,tvaleur,tcouleur);
+	}
+	else if(joueur == JOUEUR){
+		cartes[carte]=1; 
+		ajout(mainjoueur,tvaleur,tcouleur);
+	}	
 	
 	return carte;
 }
 
 // fonction qui donne la valeur d'une carte
-short donner_valeur_carte (int i){
-	
-	int carte = i;
+short donner_valeur_carte (int carte){
 	
 	if (carte == 0){
 		return 11;
 	}
-	if (carte >= 10 && carte <= 12){
+	if (carte >= 11 && carte <= 14){
 		return 10;
 	}
-	if (carte >= 1 && carte <= 9){
-		return carte+1;
+	if (carte >= 2 && carte <= 10){
+		return carte-1;
 	}
 	
 	return 0; //Check this..
 }
 
 // fonction qui donne le score d'un joueur/     
-short evaluer_score(t_main *main, int *nb_cartes, int nb_as){
+void evaluer_score(t_main * mainj){
 
 	int i, valeur_carte;
 	int points=0;
 	
-	for(i=0; i < (*nb_cartes); i++)
+	for(i=0; i < (mainj->indice); i++)
 	{
-		valeur_carte = donner_valeur_carte(main[i].valeur);
+		valeur_carte = donner_valeur_carte(mainj->carte[i].valeur);
 		points = points + valeur_carte;
 	}
 	
-	if(points > 21 && nb_as>0)
+	if(points > 21 && mainj->nb_as > 0)
 	{
-		points = points - (nb_as*10);
+		points = points - (mainj->nb_as*10);
 	}
-	return points;
+	mainj->valeurmain = points;
 	
 }
--- includes/BlackJack.h
+++ includes/BlackJack.h
@@ -8,42 +8,38 @@
 */
 // définition des différents états d'une carte du jeu de cartes
 #define LIBRE 0 // la carte est piochable
-#define BANQUE 1 // la carte est possédée par la banque et on peut la voir 
-#define JOUEUR 2 // la carte est possédée par le joueur 
-#define BANQUE_CACHEE 3 // la carte est possédée par la banque et elle n'est pas visible *
+#define PRISE 1 // la carte est dans une main
+#define CACHEE 2 // la carte est possédée mais n'est pas visible
+
+// définition des différents joueurs
+#define JOUEUR 3
+#define BANQUE 4
 
 // definition d'une énumération contenant les valeurs d'une carte // option nous sert d'indice pour nb_carte
-typedef enum {nocarte = -1, option, as, deux, trois, quatre, cinq, six, sept, huit, neuf, dix, valet, dame, roi} t_carte;
+typedef enum {as, deux, trois, quatre, cinq, six, sept, huit, neuf, dix, valet, dame, roi} t_valeur;
 
 // definition d'une énumération contenant la couleur d'une carte 
-typedef enum {nocouleur = -1, coeurs = 0, carreaux, piques, trefles} t_couleur;
+typedef enum {coeurs, carreaux, piques, trefles} t_couleur;
 
-typedef struct {t_carte valeur; t_couleur couleur;} t_main;
+typedef struct {t_valeur valeur; t_couleur couleur;} t_carte;
+
+typedef struct {t_carte carte[12]; int indice; int nb_as; int valeurmain} t_main;
 
 /* Prototypes */
-void init_pioche(short tas[52]);
-short tirer_carte(short joueur, t_main main);
+void init_game();
+void ajout(t_main * joueur, int valeur, int couleur);
+short carte_aleatoire()
+short tirer_carte(short joueur);
 void afficher_carte(short num, t_carte *main, int* nb_cartes);
-void afficher_main(t_main *main, int* nb_cartes);
-short donner_valeur_carte (int i);
-short evaluer_score(t_main *main, int *nb_cartes, int nb_as);
+short tirer_carte(short joueur);
+short donner_valeur_carte (int carte);
+void evaluer_score(t_main * mainj);
 
 // définition d'un tableau de 52 emplacements, représentant le jeu de cartes
 extern short cartes[52];
 
-// definition de la variable nb_as_joueur qui contient le nombre d'as d'un joueur
-extern int nb_as_joueur;
+// définition des structures t_main
+extern t_main mainjoueur; // déclaration de la main du joueur
+extern t_main mainbanque; // déclaration de la main de la banque
 
-// définition de la variable nb_as_banque qui contient le nombre d'as de la banque 
-extern int nb_as_banque;
-
-// définition de la variable nb_cartes_joueur qui contient le nombre de cartes du joueur
-extern int nb_cartes_joueur;
-
-// définition de la variable nb_cartes_banque qui contient le nombre de cartes de la banque
-extern int nb_cartes_banque;
-
-extern t_main mainjoueur[12]; // initialisation de la main du joueur
-extern t_main mainbanque[12]; // initialisation de la main de la banque
-
-extern t_main tas[50];
+//extern t_main tas[50];
