a360993980a3d05fcf98d81870e9e3f595cdb58a
TAHRI Ahmed <nyuubi.10@gmail.com>
2015-01-24 20:59:10 +0100

    Update to conform with ESDL 0.5.x

--- main.c
+++ main.c
@@ -16,26 +16,35 @@
 #include <string.h> //Gestion des chaines
 
 #include "includes/bj_engine.h"
+#define N 50
 
 void handleBlackJack();
-void giveawayCard(t_window * window, int idplayer);
-void handleHUD(t_window * window, char * scoreDealer, char * scorePlayer, char * betPlayer, char * soldePlayer);
+void giveawayCard(t_context * context, int idplayer);
+void handleHUD(t_context * context, char * scoreDealer, char * scorePlayer, char * betPlayer, char * soldePlayer);
 int ingameAnnouncement(char * message, SDL_Color couleur);
 
 //int nbVictoireConseq = 0;
 
 int main (int argc, char *argv[]) {
 
-	char menu_content[][M] = { "Nouvelle partie", "Mes comptes", "Quitter"};
+	char ** menu_content = (char**) malloc(N * sizeof(char*));; //{ "Nouvelle partie", "Mes comptes", "Quitter"}
+	int i = 0;
+	for (i = 0; i < N; i++)
+		menu_content[i] = (char*) malloc(N * sizeof(char));
+	
+	menu_content[0] = "Nouvelle partie";
+	menu_content[1] = "Mes comptes";
+	menu_content[2] = "Quitter";
+	
 	char name[50], mise[50], miseMin[100], miseMax[100], stats[100], ratio[100];
 	
 	int UserChoise = 0, NbEntry = 3;
     int btnOK = 0;
-    t_window * popup = NULL;
+    t_context * popup = NULL;
     
     memset(name, 0, sizeof(name));
     
-	SDL_init(800, 600, 0, "BlackJack ESDL 0.4.3", "M_ICON.png", 1, "global.ttf", 20, 1); //800x600 +tff_support +audio_support
+	SDL_initWindow(800, 600, 0, "BlackJack with ESDL 0.5", "M_ICON.png", 1, "global.ttf", 20, 1); //800x600 +tff_support +audio_support
 	joueurs[1].solde = 30000;
 	srand(time (NULL));
 	
@@ -44,17 +53,17 @@ int main (int argc, char *argv[]) {
 		//SDL_Ambiance("ambiance.wav");
 		BJ_setGameProperties((((joueurs[1].solde)/100)+1), ((joueurs[1].solde)/2), 0);
 		memset(mise, 0, sizeof(mise));
-		UserChoise = SDL_generateMenu(NbEntry, menu_content); //On affiche un menu de cinq entrée
+		UserChoise = SDL_generateMenu("app_bg.png", NbEntry, menu_content); //On affiche un menu de cinq entrée
 		
 		switch (UserChoise) {
 			
 			case 0: 
 				
-				popup = SDL_newWindow("Mise", 200, 200, 500, 250);
+				popup = SDL_newContext("Mise", 200, 200, 500, 250);
 				
-				SDL_newTexture(popup, NULL, "popup.png", 0, 0, 500, 250);
+				SDL_newImage(popup, NULL, "popup.png", 0, 0);
 				
-				SDL_newObj(popup, NULL, 1, "Mise", mise, NUMERIC,70, 70, 40, 400);
+				SDL_newObj(popup, NULL, 1, "Mise", ALIGN_LEFT, mise, NUMERIC,70, 70);
 				SDL_newText(popup, NULL, "Avant de rejoindre le salon, placez une mise.", colorWhite, 10, 40);
 				
 				sprintf(miseMin, "Mise minimale: %li euro(s)", MinimalBet);
@@ -65,8 +74,8 @@ int main (int argc, char *argv[]) {
 				SDL_newText(popup, NULL, miseMin, colorWhite, 80, 140);
 				SDL_newText(popup, NULL, miseMax, colorWhite, 80, 160);
 				
-				SDL_newObj(popup, &btnOK, 0, "OK", NULL , ALL, 10, 200, 40, 230);
-				SDL_newObj(popup, NULL, 0, "Annuler", NULL, ALL, 240, 200, 40, 230);
+				SDL_newObj(popup, &btnOK, 0, "OK", ALIGN_CENTER, NULL , NONE, 10, 200);
+				SDL_newObj(popup, NULL, 0, "Annuler", ALIGN_CENTER, NULL, NONE, 240, 200);
 				
 				//strcpy(mise, "50");
 				
@@ -80,7 +89,7 @@ int main (int argc, char *argv[]) {
 						
 						SDL_delObj(popup, 2);
 						SDL_delObj(popup, 1);
-						SDL_modObj(popup, 0, 0, "OK", NULL, ALL, 150, 200, 40, 230);
+						SDL_modObj(popup, 0, 0, "OK", ALIGN_CENTER, NULL, NONE, 150, 200);
 						SDL_delText(popup, 3);
 						SDL_delText(popup, 2);
 						SDL_delText(popup, 1);
@@ -93,17 +102,17 @@ int main (int argc, char *argv[]) {
 				
 				}
 				
-				SDL_freeWindow(popup);
+				SDL_freeContext(popup);
 				
 				break;
 				
 			case 1:
 				
-				popup = SDL_newWindow("Mes comptes", 200, 200, 500, 250);
+				popup = SDL_newContext("Mes comptes", 200, 200, 500, 250);
 				
-				SDL_newTexture(popup, NULL, "popup.png", 0, 0, 500, 250);
+				SDL_newImage(popup, NULL, "popup.png", 0, 0);
 				SDL_newText(popup, NULL, "Voici le recapitulatif de votre session", colorWhite, 10, 40);
-				SDL_newObj(popup, NULL, 1, "Pseudo", name , ALL, 70, 70, 40, 400);
+				SDL_newObj(popup, NULL, INPUT, "Pseudo", ALIGN_LEFT, name , NONE, 70, 70);
 				
 				sprintf(miseMin, "Mise minimale: %li euro(s)", MinimalBet);
 				sprintf(miseMax, "Mise maximale: %li euro(s)", MaximalBet);
@@ -115,12 +124,12 @@ int main (int argc, char *argv[]) {
 				
 				sprintf(ratio, "Ratio: %i V / %i D", nbVictoire, nbDefaite);
 				SDL_newText(popup, NULL, ratio, colorWhite, 80, 170);
-				SDL_newObj(popup, &btnOK, 0, "Fermer", NULL , ALL, 150, 200, 40, 230);
+				SDL_newObj(popup, &btnOK, BUTTON, "Fermer", ALIGN_CENTER, NULL , NONE, 150, 200);
 				
-				SDL_newSprite(popup, "panda_sp.png", colorGreenLight, 132, 144, 35, 35, 10, 100, 4, 4, 0); //Just for testing sprite
+				SDL_newSprite(popup, "panda_sp.png", colorGreenLight, 35, 35, 10, 100, 4, 4, 0); //Just for testing sprite
 				
 				SDL_generate(popup);
-				SDL_freeWindow(popup);
+				SDL_freeContext(popup);
 				
 				break;
 			
@@ -142,11 +151,18 @@ int main (int argc, char *argv[]) {
 
 void handleBlackJack() {
 
-	t_window * ingame = SDL_newWindow("BlackJack", 0, 0, 800, 600);
+	t_context * ingame = SDL_newContext("BlackJack", 0, 0, 800, 600);
 	char scoreDealer[50], scorePlayer[50], betPlayer[50], soldePlayer[50];
 	int choise = 0;
 	
-	SDL_newTexture(ingame, NULL, "app_bg_ingame.png", 0, 0, 800, 600);
+	SDL_loadSound("Female/killingspree.wav");
+	SDL_loadSound("lose.wav");
+	SDL_loadSound("gotblackjack.wav");
+	SDL_loadSound("Female/dominating.wav");
+	SDL_loadSound("aplause.wav");
+	SDL_loadSound("deal.wav");
+	
+	SDL_newImage(ingame, NULL, "app_bg_ingame.png", 0, 0);
 	handleHUD(ingame, scoreDealer, scorePlayer, betPlayer, soldePlayer);
 	BJ_setCards(); //Prepare cards 
 	
@@ -174,17 +190,17 @@ void handleBlackJack() {
 		nbVictoireConseq++;
 		nbVictoire++;
 		
-		SDL_playSound("Female/killingspree.wav", 0);
+		SDL_playSound("Female/killingspree.wav");
 		
 		BJ_setMonney(1, ((joueurs[1].mise)*2)+((joueurs[1].mise)/2));
 		ingameAnnouncement("BlackJack au service..! Pas mal.", colorWhite);
-		SDL_freeWindow(ingame);
+		SDL_freeContext(ingame);
 		return;
 	}
 	
-	SDL_newObj(ingame, NULL, 0, "Abandon", NULL , ALL, 50, 530, 40, 230);
-	SDL_newObj(ingame, NULL, 0, "Rester", NULL, ALL, 280, 530, 40, 230);
-	SDL_newObj(ingame, NULL, 0, "Frapper", NULL, ALL, 510, 530, 40, 230);
+	SDL_newObj(ingame, NULL, 0, "Abandon", ALIGN_CENTER, NULL , NONE, 50, 530);
+	SDL_newObj(ingame, NULL, 0, "Rester", ALIGN_CENTER, NULL, NONE, 280, 530);
+	SDL_newObj(ingame, NULL, 0, "Frapper", ALIGN_CENTER, NULL, NONE, 510, 530);
 	
 	choise = SDL_generate(ingame);
 	
@@ -201,13 +217,13 @@ void handleBlackJack() {
 			SDL_delObj(ingame, 0);
 			SDL_generate(ingame);
 			
-			SDL_playSound("lose.wav", 0);
+			SDL_playSound("lose.wav");
 			
 			nbVictoireConseq = 0;
 			nbDefaite++;
 			SDL_Delay(1000);
 			ingameAnnouncement("T'es crame, tu depasse 21 mon vieux.", colorRed);
-			SDL_freeWindow(ingame);
+			SDL_freeContext(ingame);
 			
 			return;
 			
@@ -220,12 +236,12 @@ void handleBlackJack() {
 			nbVictoireConseq++;
 			nbVictoire++;
 			
-			SDL_playSound("gotblackjack.wav", 0);
+			SDL_playSound("gotblackjack.wav");
 			
 			SDL_Delay(1000);
 			BJ_setMonney(1, (joueurs[1].mise)*2);
 			ingameAnnouncement("BlackJack ! Bon jeu..! Revenez, la prochaine je gagne..", colorWhite);
-			SDL_freeWindow(ingame);
+			SDL_freeContext(ingame);
 			
 			return;
 			
@@ -240,7 +256,7 @@ void handleBlackJack() {
 		nbVictoireConseq = 0;
 		nbDefaite++;
 		ingameAnnouncement("Bonne ou mauvaise idee, tu ne le saura jamais.", colorRed);
-		SDL_freeWindow(ingame);
+		SDL_freeContext(ingame);
 		return;
 	}
 	
@@ -261,11 +277,11 @@ void handleBlackJack() {
 		
 		
 		if (nbVictoireConseq == 2) {
-			SDL_playSound("Female/dominating.wav", 0);
+			SDL_playSound("Female/dominating.wav");
 		}else if(nbVictoireConseq > 2) {
-			SDL_playSound("Female/unstoppable.wav", 0);
+			SDL_playSound("Female/unstoppable.wav");
 		}else {
-			SDL_playSound("aplause.wav", 0);
+			SDL_playSound("aplause.wav");
 		}
 		
 		SDL_Delay(1000);
@@ -273,26 +289,26 @@ void handleBlackJack() {
 		nbVictoire++;
 		BJ_setMonney(1, (joueurs[1].mise)*2);
 		ingameAnnouncement("Je me suis crame, une revanche ?", colorWhite);
-		SDL_freeWindow(ingame);
+		SDL_freeContext(ingame);
 		
 	}else if(BJ_getScore(0) > BJ_getScore(1)) {
 		
-		SDL_playSound("lose.wav", 0);
+		SDL_playSound("lose.wav");
 		
 		nbVictoireConseq = 0;
 		nbDefaite++;
 		SDL_Delay(1000);
 		ingameAnnouncement("Les jeux sont fait, n'hesite pas a revenir !", colorRed);
-		SDL_freeWindow(ingame);
+		SDL_freeContext(ingame);
 		
 	}else if(BJ_getScore(0) < BJ_getScore(1)) {
 		
 		if (nbVictoireConseq == 2) {
-			SDL_playSound("Female/dominating.wav", 0);
+			SDL_playSound("Female/dominating.wav");
 		}else if(nbVictoireConseq > 2) {
-			SDL_playSound("Female/unstoppable.wav", 0);
+			SDL_playSound("Female/unstoppable.wav");
 		}else {
-			SDL_playSound("aplause.wav", 0);
+			SDL_playSound("aplause.wav");
 		}
 		
 		
@@ -300,14 +316,14 @@ void handleBlackJack() {
 		nbVictoire++;
 		BJ_setMonney(1, (joueurs[1].mise)*2);
 		ingameAnnouncement("Pas mal du tout !", colorWhite);
-		SDL_freeWindow(ingame);
+		SDL_freeContext(ingame);
 		
 	}else if(BJ_getScore(0) == BJ_getScore(1)) {
 		
 		SDL_Delay(1000);
 		BJ_setMonney(1, joueurs[1].mise);
 		ingameAnnouncement("Sa ce termine sur une belle egalite..! Revanche ?", colorWhite);
-		SDL_freeWindow(ingame);
+		SDL_freeContext(ingame);
 		
 	}
 	
@@ -315,25 +331,25 @@ void handleBlackJack() {
 
 int ingameAnnouncement(char * message, SDL_Color couleur) {
 	
-	t_window * popup = SDL_newWindow("Mes comptes", 200, 200, 500, 250);
+	t_context * popup = SDL_newContext("Mes comptes", 200, 200, 500, 250);
 	char ratio[50];
 	
-	SDL_newTexture(popup, NULL, "popup.png", 0, 0, 500, 250);
+	SDL_newImage(popup, NULL, "popup.png", 0, 0);
 	
 	SDL_newText(popup, NULL, message, couleur, 50, 100);
 	sprintf(ratio, "Ratio: %i V / %i D", nbVictoire, nbDefaite);
 	SDL_newText(popup, NULL, ratio, colorWhite, 50, 120);
 	
-	SDL_newObj(popup, NULL, 0, "OK", NULL , ALL, 150, 200, 40, 230);
+	SDL_newObj(popup, NULL, 0, "OK", ALIGN_CENTER, NULL , NONE, 150, 200);
 	
 	SDL_generate(popup);
-	SDL_freeWindow(popup);
+	SDL_freeContext(popup);
 	
 	return 1;
 	
 }
 
-void giveawayCard(t_window * window, int idplayer) {
+void giveawayCard(t_context * context, int idplayer) {
 	
 	char * fileName = calloc(50, sizeof(char));
 	//memset(fileName, 0, sizeof(char)*50);
@@ -341,17 +357,17 @@ void giveawayCard(t_window * window, int idplayer) {
 	/*Begin, give away card to idplayer*/
 	BJ_attrCard(idplayer); 
 	
-	SDL_playSound("deal.wav", 0);
+	SDL_playSound("deal.wav");
 	
 	BJ_getCardfilename(idplayer, (joueurs[idplayer].nbCard-1), fileName);
 	
 	switch (idplayer) {
 		
 		case 0:
-			SDL_newTexture(window, NULL, fileName, 350+((joueurs[idplayer].nbCard-1)*30), 100, 71, 96);
+			SDL_newImage(context, NULL, fileName, 350+((joueurs[idplayer].nbCard-1)*30), 100);
 			break;
 		case 1:
-			SDL_newTexture(window, NULL, fileName, 350+((joueurs[idplayer].nbCard-1)*30), 380, 71, 96);
+			SDL_newImage(context, NULL, fileName, 350+((joueurs[idplayer].nbCard-1)*30), 380);
 			break;
 			
 	}
@@ -360,44 +376,44 @@ void giveawayCard(t_window * window, int idplayer) {
 	
 }
 
-void handleHUD(t_window * window, char * scoreDealer, char * scorePlayer, char * betPlayer, char * soldePlayer) {
+void handleHUD(t_context * context, char * scoreDealer, char * scorePlayer, char * betPlayer, char * soldePlayer) {
 	
 	sprintf(scoreDealer, "%i", BJ_getScore(0));
 	sprintf(scorePlayer, "%i", BJ_getScore(1));
 	sprintf(betPlayer, "%li euro(s)", joueurs[1].mise);
 	sprintf(soldePlayer, "%li", joueurs[1].solde);
 	
-	if (window->nbText == 0) {
+	if (context->nbText == 0) {
 		
-		SDL_newText(window, NULL, scoreDealer, colorWhite, 330, 100);
-		SDL_newText(window, NULL, scorePlayer, colorWhite, 330, 380);
-		SDL_newText(window, NULL, betPlayer, colorWhite, 350, 360);
-		SDL_newText(window, NULL, soldePlayer, colorWhite, 115, 580);
+		SDL_newText(context, NULL, scoreDealer, colorWhite, 330, 100);
+		SDL_newText(context, NULL, scorePlayer, colorWhite, 330, 380);
+		SDL_newText(context, NULL, betPlayer, colorWhite, 350, 360);
+		SDL_newText(context, NULL, soldePlayer, colorWhite, 115, 580);
 		
 	}else{
 		
 		if (BJ_getScore(0) > 21) {
 		
-			SDL_modText(window, 0, scoreDealer, colorRed, 330, 100);
+			SDL_modText(context, 0, scoreDealer, colorRed, 330, 100);
 			
 		}else{
 		
-			SDL_modText(window, 0, scoreDealer, colorWhite, 330, 100);
+			SDL_modText(context, 0, scoreDealer, colorWhite, 330, 100);
 			
 		}
 		
 		if (BJ_getScore(1) > 21) {
 		
-			SDL_modText(window, 1, scorePlayer, colorRed, 330, 380);
+			SDL_modText(context, 1, scorePlayer, colorRed, 330, 380);
 			
 		}else{
 		
-			SDL_modText(window, 1, scorePlayer, colorWhite, 330, 380);
+			SDL_modText(context, 1, scorePlayer, colorWhite, 330, 380);
 			
 		}
 		
-		SDL_modText(window, 2, betPlayer, colorWhite, 350, 360);
-		SDL_modText(window, 3, soldePlayer, colorWhite, 115, 580);
+		SDL_modText(context, 2, betPlayer, colorWhite, 350, 360);
+		SDL_modText(context, 3, soldePlayer, colorWhite, 115, 580);
 		
 	}
 
